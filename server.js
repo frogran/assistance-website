8860a3283ecec7a74049e74fa5424394bf8088cd
frogran <gorfung@gmail.com>
2024-09-16 00:08:56 +0300

    more firebase attempts

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ const PORT = process.env.PORT || 3000;
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://reinforcement-c359d.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.database();
